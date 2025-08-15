f318373bf13044c26102340df42a6cf5fa9d8fef
Matttgic <matthieu.gonthier@gmail.com>
2025-08-15 19:59:21 +0200

    Update league_mapping.py

--- config/league_mapping.py
+++ config/league_mapping.py
@@ -1,37 +1,36 @@
 # config/league_mapping.py
 LEAGUE_CODE_TO_API_ID = {
-    # Angleterre
-    "E0": 39,   # Premier League
-    "E1": 40,   # Championship
+    # Europe - Ligues principales 2024-25
+    "E0": 39,   # Premier League (Angleterre)
+    "E1": 40,   # Championship (Angleterre)
+    "SP1": 140, # La Liga (Espagne)
+    "SP2": 141, # Segunda División (Espagne)
+    "F1": 61,   # Ligue 1 (France)
+    "F2": 62,   # Ligue 2 (France)
+    "I1": 135,  # Serie A (Italie)
+    "I2": 136,  # Serie B (Italie)
+    "D1": 78,   # Bundesliga (Allemagne)
+    "D2": 79,   # 2. Bundesliga (Allemagne)
+    "N1": 88,   # Eredivisie (Pays-Bas)
+    "B1": 144,  # Jupiler Pro League (Belgique)
+    "P1": 94,   # Primeira Liga (Portugal)
+    "T1": 203,  # Super Lig (Turquie)
+    "G1": 197,  # Super League (Grèce)
     
-    # Espagne
-    "SP1": 140, # La Liga
-    "SP2": 141, # Segunda División
-    
-    # France
-    "F1": 61,   # Ligue 1
-    "F2": 62,   # Ligue 2
-    
-    # Italie
-    "I1": 135,  # Serie A
-    "I2": 136,  # Serie B
-    
-    # Allemagne
-    "D1": 78,   # Bundesliga
-    "D2": 79,   # 2. Bundesliga
-    
-    # Pays-Bas
-    "N1": 88,   # Eredivisie
-    
-    # Belgique
-    "B1": 144,  # Jupiler Pro League
-    
-    # Portugal
-    "P1": 94,   # Primeira Liga
-    
-    # Turquie
-    "T1": 203,  # Super Lig
-    
-    # Grèce
-    "G1": 197,  # Super League
+    # Ligues mondiales - "current" season
+    "ARG": 128, # Liga Profesional Argentina
+    "AUT": 218, # Tipp3 Bundesliga (Autriche)
+    "BRA": 71,  # Serie A (Brésil)
+    "DNK": 119, # Superligaen (Danemark)
+    "CHN": 169, # Chinese Super League (estimation basée sur votre config)
+    "FIN": 244, # Veikkausliiga (Finlande)
+    "IRL": 357, # Premier Division (Irlande)
+    "JPN": 98,  # J1 League (estimation basée sur votre config)
+    "MEX": 262, # Liga MX (Mexique)
+    "NOR": 103, # Eliteserien (Norvège)
+    "POL": 106, # Ekstraklasa (Pologne)
+    "ROU": 283, # Liga I (Roumanie)
+    "SWE": 113, # Allsvenskan (Suède)
+    "SWZ": 207, # Super League (Suisse) - ID différent de la Grèce
+    "USA": 253, # Major League Soccer (MLS)
 }
