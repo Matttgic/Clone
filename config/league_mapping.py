2612d613d1f975cbad26d7629a912fb699d2cd4f
Matttgic <matthieu.gonthier@gmail.com>
2025-08-15 19:57:52 +0200

    Create league_mapping.py

---
+++ config/league_mapping.py
@@ -0,0 +1,37 @@
+# config/league_mapping.py
+LEAGUE_CODE_TO_API_ID = {
+    # Angleterre
+    "E0": 39,   # Premier League
+    "E1": 40,   # Championship
+    
+    # Espagne
+    "SP1": 140, # La Liga
+    "SP2": 141, # Segunda División
+    
+    # France
+    "F1": 61,   # Ligue 1
+    "F2": 62,   # Ligue 2
+    
+    # Italie
+    "I1": 135,  # Serie A
+    "I2": 136,  # Serie B
+    
+    # Allemagne
+    "D1": 78,   # Bundesliga
+    "D2": 79,   # 2. Bundesliga
+    
+    # Pays-Bas
+    "N1": 88,   # Eredivisie
+    
+    # Belgique
+    "B1": 144,  # Jupiler Pro League
+    
+    # Portugal
+    "P1": 94,   # Primeira Liga
+    
+    # Turquie
+    "T1": 203,  # Super Lig
+    
+    # Grèce
+    "G1": 197,  # Super League
+}
